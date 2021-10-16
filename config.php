4b2712a64335c6a7fd5aa8b9bb9a243d590d260a
LucasLm2 <lucas_pss13@hotmail.com>
2021-10-16 11:34:31 -0300

    preparação para inclusão no dominio modelo

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php 
 	define('INCLUDE_PATH_SITE','http://localhost/bcode-site-antigo/');
-	// define('INCLUDE_PATH_SITE','https://bcode-antigo.passosti.com');
+	// define('INCLUDE_PATH_SITE','https://bcode-antigo.passosti.com/');
 	require_once('vendor/autoload.php');
 ?>
